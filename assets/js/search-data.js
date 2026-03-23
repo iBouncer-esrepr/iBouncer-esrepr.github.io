6f77aaa8aeef060f720fedf291df8c74b696fcc0
iBouncer-esrepr <y.saito.epr@gmail.com>
2026-03-23 09:39:07 +0000

    Deploying to gh-pages from @ iBouncer-esrepr/iBouncer-esrepr.github.io@c3de267c2c383d528af4d7bd228620cec924e981 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,21 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-新サイト開設のお知らせ",
+        },{id: "post-新サイト開設のお知らせ",
         
           title: "新サイト開設のお知らせ",
         
