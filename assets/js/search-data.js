bfd7e6c8db691e0f990d050e0a927d7118790d8a
iBouncer-esrepr <y.saito.epr@gmail.com>
2026-03-23 06:26:52 +0000

    Deploying to gh-pages from @ iBouncer-esrepr/iBouncer-esrepr.github.io@f23ce2e91ff3e8eccce874f39759f65acc87136a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -103,9 +103,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-antioxidant-capacity-evaluation-based-on-scientific-evidence-and-methodology",
+          title: 'Antioxidant Capacity Evaluation based on Scientific Evidence and Methodology.',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "teachings-data-science-fundamentals",
