42ff2e5e042331addf08ab42005af0a118afdcdf
iBouncer-esrepr <y.saito.epr@gmail.com>
2026-03-27 03:09:36 +0000

    Deploying to gh-pages from @ iBouncer-esrepr/iBouncer-esrepr.github.io@b2c4f23e9ec2956432c9a02b7a6b3b296517cc91 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "",
+          description: "Yu Saito&#39;s Curriculum Vitae",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
