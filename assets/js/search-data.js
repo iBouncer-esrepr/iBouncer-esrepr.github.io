b214eb88f2cb1e9af7163ca1778202b5265e1552
iBouncer-esrepr <y.saito.epr@gmail.com>
2026-03-27 01:43:47 +0000

    Deploying to gh-pages from @ iBouncer-esrepr/iBouncer-esrepr.github.io@bbb29dd371c1d53fb009eecf73aae10860f7b8f0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,19 +23,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
-          },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
           },
         },{id: "post-新サイト開設のお知らせ",
         
